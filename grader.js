25187c0ffcf77930e75f84f6f44d1f4475bffe88
Matt Morris <matt@modbp.com>
2013-07-12 03:40:19 +0000

    Switching grader.js to use a URL if possible

--- grader.js
+++ grader.js
@@ -22,8 +22,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -33,6 +35,12 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var getUrl = function(inurl){
+    rest.get(inurl).on('complete', function(data, response) {
+       return data;
+   }
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -63,8 +71,12 @@ if(require.main == module){
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <html_url>', 'URL to index.html', getUrl, URLFILE_DEFAULT);
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var htmlfile = program.file != null 
+	     ? program.file
+             : program.url;
+    var checkJson = checkHtmlFile(htmlfile, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
